f9099852e29a5acb90fd617abdaf8f4f8f95dba6
abailey76 <abailey76@duck.com>
2021-12-08 14:24:13 -0600

    reviewing code

--- TechJobsOO/Location.cs
+++ TechJobsOO/Location.cs
@@ -10,7 +10,7 @@
         }
 
         // TODO: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
-        public Location(string v) : base(v)
+        public Location(string value) : base(value)
         {
 
 
